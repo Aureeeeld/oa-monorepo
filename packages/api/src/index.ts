181a2ce8986d559b7561c50ee9e901408b8d0c56
Nolat <NolatYT@gmail.com>
2019-11-02 19:58:12 +0100

    :sparkles: (API-1): Add simple authorization

--- packages/api/src/index.ts
+++ packages/api/src/index.ts
@@ -9,16 +9,21 @@ import { UserResolver } from "./resolvers";
 import "./lib/env";
 
 const port: number = parseInt(process.env.PORT!, 10);
+const secretToken: string = process.env.SECRET_TOKEN!;
 
 // * Bootstrap GraphQL Server & DB connection
 async function bootstrapServer() {
   const schema = await buildSchema({
     resolvers: [UserResolver],
+    authChecker: ({ context: { req } }) => {
+      return req.headers.authorization === secretToken;
+    },
     emitSchemaFile: false
   });
 
   const server = new ApolloServer({
-    schema
+    schema,
+    context: ({ req }) => ({ req })
   });
 
   createConnection()
--- packages/api/src/resolvers/UserResolver.ts
+++ packages/api/src/resolvers/UserResolver.ts
@@ -1,15 +1,17 @@
-import { Query, Mutation, Resolver, Arg } from "type-graphql";
+import { Authorized, Query, Mutation, Resolver, Arg } from "type-graphql";
 
 import { User } from "../entities/User";
 import UserSchema from "../schemas/UserSchema";
 
 @Resolver(of => UserSchema)
 export default class {
+  @Authorized()
   @Query(returns => [UserSchema])
   users() {
     return User.find();
   }
 
+  @Authorized()
   @Mutation(returns => UserSchema)
   async createUser(@Arg("name") name: string): Promise<User> {
     const user = new User();
