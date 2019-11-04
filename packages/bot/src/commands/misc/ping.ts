84ee699eab78852f46ff6afd369557ade711c23f
Alex Amemoutou <mr.say6@gmail.com>
2019-11-04 17:03:27 +0100

    :sparkles: (BOT): Add ping command

---
+++ packages/bot/src/commands/misc/ping.ts
@@ -0,0 +1,24 @@
+import { Message, RichEmbed } from "discord.js";
+import moment from "moment";
+import { Command } from "../types";
+
+const PingCommand: Command = {
+  name: "ping",
+  aliases: [""],
+  usage: "",
+  description: "Affiche le temps de réponse du bot",
+  onlyStaff: false,
+  run: (message: Message) => {
+    const embed: RichEmbed = new RichEmbed();
+
+    const time = moment().diff(message.createdTimestamp);
+    embed
+      .setColor("BLUE")
+      .setTitle("Ping")
+      .setDescription(`${time}ms`);
+
+    message.channel.send(embed);
+  }
+};
+
+export default PingCommand;
